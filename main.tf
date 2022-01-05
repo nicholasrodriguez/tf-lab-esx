23ae5e3befbcbaf74b8a6da3462c4be1534ec651
nicholasrodriguez <nicholasrodriguez@users.noreply.github.com>
2022-01-05 13:55:51 +0000

    Added versions and lock file Ran fmt

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    esxi = {
-      source = "registry.terraform.io/josenk/esxi"
-      #
-      # For more information, see the provider source documentation:
-      # https://github.com/josenk/terraform-provider-esxi
-      # https://registry.terraform.io/providers/josenk/esxi
-    }
-  }
-}
-
 provider "esxi" {
   esxi_hostname = "esx02"
   esxi_hostport = "22"
@@ -22,11 +9,6 @@ provider "esxi" {
 resource "esxi_guest" "vmtest" {
   guest_name = "vmtest"
   disk_store = "esx02_das01"
-  #
-  #  Specify an existing guest to clone, an ovf source, or neither to build a bare-metal guest vm.
-  #
-  #clone_from_vm      = "Templates/centos7"
-  #ovf_source        = "/local_path/centos-7.vmx"
   network_interfaces {
     virtual_network = "lab_dev"
   }
